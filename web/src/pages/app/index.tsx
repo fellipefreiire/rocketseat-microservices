7bb146b0d1b690a8cf2ad34f6ee977a63b6a216e
Felipe <fellipefreiire3@gmail.com>
2022-04-27 02:28:47 -0300

    frontend config done

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,11 +1,19 @@
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0'
 import type { GetServerSideProps, NextPage } from 'next'
+import { useMeQuery } from '../../graphql/generated/graphql'
+import { getServerPageGetProducts, ssrGetProducts } from '../../graphql/generated/page'
+import { withApollo } from '../../lib/withApollo'
 
 const Home: NextPage = () => {
   const { user } = useUser()
+  const { data: me } = useMeQuery()
+
   return (
-    <div>
+    <div className="text-violet-500">
       <h1>Hello World</h1>
+      <pre>
+        {JSON.stringify(me)}
+      </pre>
       <pre>
         {JSON.stringify(user, null, 2)}
       </pre>
@@ -14,7 +22,15 @@ const Home: NextPage = () => {
     </div>
   )
 }
+export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
+  getServerSideProps: async (ctx) => {
+    return {
+      props: {}
+    }
+    // return getServerPageGetProducts({}, ctx)
+  }
+})
 
-export default Home
-
-export const getServerSideProps: GetServerSideProps = withPageAuthRequired()
+export default withApollo(
+  ssrGetProducts.withPage()(Home)
+)
